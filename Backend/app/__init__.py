019042ab52d1fbd2263df5458516a860ea006147
RichardLiang13 <1760268004@qq.com>
2025-01-08 15:08:44 +0800

    Modify:Use flask_cors to resolve cross-origin issues.

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+# 启用启用 Flask-CORS，允许前端跨域请求
+from flask_cors import CORS
 from app.extensions import init_extensions
 from app.utils.logger import setup_logger
 
 def create_app(config_class='app.config.DevelopmentConfig'):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    # 启用全局跨域支持
+    CORS(app)  # 允许所有域名跨域
 
     # 初始化扩展
     init_extensions(app)
